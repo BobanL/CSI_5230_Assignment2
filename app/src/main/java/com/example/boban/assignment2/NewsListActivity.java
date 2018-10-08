36adeeb1023dd84b2e7124976108deefebdd386d
Boban <ljuljdjurovic@gmail.com>
2018-10-08 17:22:47 -0400

    News List Complete

---
+++ app/src/main/java/com/example/boban/assignment2/NewsListActivity.java
@@ -0,0 +1,13 @@
+package com.example.boban.assignment2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_list);
+    }
+}
